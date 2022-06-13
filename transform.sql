3302e4e790bddc24d9d2397c5a26e4993eaf86f4
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-06-13 17:01:23 -0400

    Added insert into series

--- transform.sql
+++ transform.sql
@@ -21,3 +21,15 @@ select m.oai_id, dc_identifier_sid, dc_title, dc_creator, dc_description,
   where length(dc_identifier_sid) =
           length(replace(dc_identifier_sid, '-', '')) + 2 and
         dc_identifier_sid not like 'S-%';
+
+-- series
+insert into un_archives.series
+  (series_id, un_id, title, creator, description,
+   url, record_created, fond_id)
+select m.oai_id, dc_identifier_sid, dc_title, dc_creator, dc_description,
+       dc_identifier_uri, oai_timestamp, m.oai_set
+  from un_archives.metadata m join un_archives.sets s
+      on (m.oai_set = s.oai_id)
+  where length(dc_identifier_sid) =
+          length(replace(dc_identifier_sid, '-', '')) + 1 and
+        dc_identifier_sid like 'S-%';
