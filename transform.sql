e9677546a157b4878a201acd41bc6d16f81a418d
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-06-13 18:42:00 -0400

    Added insert into folders

--- transform.sql
+++ transform.sql
@@ -33,3 +33,18 @@ select m.oai_id, dc_identifier_sid, dc_title, dc_creator, dc_description,
   where length(dc_identifier_sid) =
           length(replace(dc_identifier_sid, '-', '')) + 1 and
         dc_identifier_sid like 'S-%';
+
+-- folder
+insert into un_archives.folders
+  (folder_id, un_id, series_id,
+   title, description, url, classification, record_created)
+select m.oai_id, m.dc_identifier_sid,
+       (select series_id from un_archives.series
+           where un_id = substring(m.dc_identifier_sid, 1, 6)),
+       dc_title, dc_description, dc_identifier_uri,
+       substring(lower(dc_rights), 17), oai_timestamp
+  from un_archives.metadata m join un_archives.sets s
+      on (m.oai_set = s.oai_id)
+  where length(dc_identifier_sid) =
+          length(replace(dc_identifier_sid, '-', '')) + 3 and
+        dc_identifier_sid like 'S-%';
